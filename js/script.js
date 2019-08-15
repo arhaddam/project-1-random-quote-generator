03cbed3ec917df2809926a35ebe36e9906eb81b6
arhaddam <ar@haddam.tech>
2019-08-14 20:37:13 -0400

    final update to project 1

--- js/script.js
+++ js/script.js
@@ -1,56 +1,90 @@
 /******************************************
 Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
+Author: Abdul-Rahman Haddam
+Created Date: 8/14/2019
+
+Summary: Displays random famous quotes each time a button is clicked. 
+
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
+// create quotes variable array to store quotes in objects. 
+
+var quotes = [
+  {quote: "Live as if you were to die tomorrow. Learn as if you were to live forever.", 
+    source: "Mahatma Gandhi",
+    tag: "Inspirational"
+  },
+  {quote: "Do what you can, with what you have, where you are.", 
+    source: "Theodore Roosevelt",
+    tag: "Inspirational"
+  },
+  {quote: "Wise men speak because they have something to say; fools because they have to say something.", 
+    source: "Plato",
+    tag: "Inspirational"
+  },
+  {quote: "You don't win friends with salad", 
+    source: "Homer Simpson", 
+    tag: "Humor",
+    citation: "Lisa the Vegetarian (Season 7, Episode 5)"
+  },
+  {quote: "My fellow Americans, ask not what your country can do for you, ask what you can do for your country.", 
+    source: "John F. Kennedy", 
+    tag: "Inspirational",
+    year: "January 20, 1961" 
+  }
+]
 
 
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
+// function returns a single random quote from our quotes array
+
+function getRandomQuote() {
+  var randomNumber = Math.floor(Math.random() * quotes.length);
+  return quotes[randomNumber];
+}
 
 
+// function prints quote from our getRandomQuote() function into the quote-box div
+
+function printQuote() {
+  var quote = getRandomQuote();
+  var html = '';
+  html += '<p class="quote">'+quote.quote+'</p>';
+  html += '<p class="source">'+quote.source;
+  if(quote.citation) {
+    html += '<span class="citation">'+quote.citation+'</span>';
+  }
+  if(quote.year) {
+
+    html += '<span class="year">'+quote.year+'</span>';
+  }
+  html += '<span class="tag">'+quote.tag+'</span></p>';
+  document.getElementById("quote-box").innerHTML = html;
+  setRandomColor();
+}
 
 
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
+// function creates 3 random numbers,, sets them to a variable, and updates background color of the body
 
+function setRandomColor() {
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  var bgColor = "rgb(" + r + "," + g + "," + b + ")";
 
-
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-
+  document.body.style.background = bgColor;
+}
 
 
 /***
   When the "Show another quote" button is clicked, the event listener 
   below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
+  function. 
 ***/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+// invoke printQuote function every 20 seconds
+
+setInterval(printQuote, 20000);
